80a7360e7a7517d7e4529486d67b294e1f74b984
kthong3 <kthongsakounh@gmail.com>
2017-12-12 00:22:57 -0600

    create other contacts section and update css

---
+++ app/helpers/contact_helper.rb
@@ -0,0 +1,7 @@
+def is_favorite(contact)
+  contact["isFavorite"] == true
+end
+
+def other_contact(contact)
+  contact["isFavorite"] == false
+end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,3 @@
 class Contact < ActiveRecord::Base
 
-  def parse_contact(contact)
-    contact.
-  end
 end
